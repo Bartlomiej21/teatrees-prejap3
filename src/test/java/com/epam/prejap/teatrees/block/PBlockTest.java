5c679cd69c1948eabcf57b742ad90dfb9384600f
Bartłomiej Słomiński <slominskibartlomiej@wp.pl>
2022-02-02 23:19:08 +0100

    Replaced the tests on unhappy path. Now they use instance of PBlock instead of raw data and test whether correct exceptions are thrown

--- src/test/java/com/epam/prejap/teatrees/block/PBlockTest.java
+++ src/test/java/com/epam/prejap/teatrees/block/PBlockTest.java
@@ -2,6 +2,7 @@ package com.epam.prejap.teatrees.block;
 
 import static org.testng.Assert.*;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class PBlockTest {
 
@@ -10,20 +11,43 @@ public class PBlockTest {
             {1, 1},
             {1, 0},
     };
-    final byte[][] WRONG_IMAGE = {
-            {1, 1, 1},
-            {1, 5},
+    final byte[][] WRONG_IMAGE = {};
+    final byte[][] WRONG_IMAGE2 = {
+            {1, 1},
+            {1, 1},
             {1, 0},
-            {},
+            {1,}
     };
+    final byte[][] WRONG_IMAGE3 = {
+            {1, 1},
+            {1, -1},
+            {1, 0},
+            {0, 1},
+    };
+
+
+    //PBlock image1 = new PBlock(IMAGE);
+    PBlock image1 = new PBlock();
 
     //HAPPY PATH
     @Test
     void checkIfNoRowsAreVoid(){
-        for (int i=0; i<IMAGE.length;i++){
-            assertTrue(IMAGE[i].length>0);
+        SoftAssert softAssertion= new SoftAssert();
+        for (int i=0; i<image1.rows;i++){
+            //assertTrue(image1.rows>0);
+            //assertTrue(image1.dotAt(i,0)!=assertNotNull();
+            softAssertion.assertNotNull(image1.dotAt(i,0));
         }
+        softAssertion.assertAll();
     }
+
+    /*
+    public int rows() {return rows;}
+    public int cols() {
+        return cols;
+    }
+    public byte dotAt(int i, int j) { return image[i][j]; }
+     */
 
     @Test
     void checkIfBlockFieldsContainOnlyOnesAndZeroes(){
@@ -53,37 +77,34 @@ public class PBlockTest {
     }
 
     //NOT SO HAPPY PATH
-    @Test
-    void checkIfVoidRowsAreDetected(){
-        for (int i=0; i<WRONG_IMAGE.length;i++){
-            assertTrue(WRONG_IMAGE[i].length>0);
+    @Test (expectedExceptions = {IllegalArgumentException.class}, expectedExceptionsMessageRegExp = "Image has height equal to 0")
+    void checkIfBlockWithZeroHeightIsCaught() {
+        try {
+            new PBlock(WRONG_IMAGE);
+        }
+        catch (Exception e){
+            System.out.println(e);
+            throw e;
         }
     }
-
-    @Test
-    void checkIfBlockFieldsContainOnlyOnesAndZeroesAreDetected(){
-        for (int i=0; i<WRONG_IMAGE.length; i++){
-            for (int j=0; j<WRONG_IMAGE[0].length;j++) {
-                byte field = WRONG_IMAGE[i][j];
-                assertTrue(field>=0 && field <=1 );
-            }
+    @Test (expectedExceptions = {IllegalArgumentException.class}, expectedExceptionsMessageRegExp = "Image is not a rectangle")
+    void checkIfBlockThatIsNotARectangleIsCaught() {
+        try {
+            new PBlock(WRONG_IMAGE2);
+        }
+        catch (Exception e){
+            System.out.println(e);
+            throw e; //IllegalArgumentException(e);
         }
     }
-
-    @Test
-    void checkIfIncorrectRowLengthIsDetected(){
-        int columns = WRONG_IMAGE[0].length;
-        for (int i=0; i<WRONG_IMAGE.length; i++){
-            assertEquals(WRONG_IMAGE[i].length,columns);
+    @Test (expectedExceptions = {IllegalArgumentException.class}, expectedExceptionsMessageRegExp = "Invalid dot value")
+    void checkIfWrongDotValueIsDetected() {
+        try {
+            new PBlock(WRONG_IMAGE3);
         }
-    }
-
-    @Test
-    void checkIfBlocksThatAreNotRectangularsWillPass(){
-        //every line should have the same length
-        int rowLength = WRONG_IMAGE[0].length;
-        for (int i=0; i<WRONG_IMAGE.length; i++){
-            assertEquals(WRONG_IMAGE[i].length,rowLength);
+        catch (Exception e){
+            System.out.println(e);
+            throw e;
         }
     }
 }
